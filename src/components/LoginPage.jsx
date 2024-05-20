9df3efa45da0bbd7f8d0050d96d4f6776b87f048
Dikshant Thakur <dikshantthakur.nith@gmail.com>
2024-05-20 11:02:26 +0530

    first commit

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <>
+      <h2>Login</h2>
+    </>
+  );
+}
+
+export default LoginPage;
